b4fdab2bf574e440d3bd41bd9d1ffdc211f3c1de
Mike Ledger <eleventynine@gmail.com>
2020-07-21 16:33:46 +1000

    rollup warning told me so

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import pkg from './package.json';
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'cjs' },
+    { file: pkg.main, format: 'cjs', exports: 'named' },
     { file: pkg.module, format: 'es' },
   ],
   plugins: [
