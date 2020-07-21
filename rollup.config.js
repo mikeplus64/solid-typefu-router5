53943bbb0c6394553be51d898382cf7c575cfe34
Mike Ledger <eleventynine@gmail.com>
2020-07-21 18:11:21 +1000

    try have sourcemap

--- rollup.config.js
+++ rollup.config.js
@@ -5,8 +5,8 @@ import pkg from './package.json';
 export default {
   input: 'src/index.ts',
   output: [
-    { file: pkg.main, format: 'cjs', exports: 'named' },
-    { file: pkg.module, format: 'es' },
+    { file: pkg.main, format: 'cjs', exports: 'named', sourcemap: true },
+    { file: pkg.module, format: 'es', sourcemap: true },
   ],
   plugins: [
     typescript({
